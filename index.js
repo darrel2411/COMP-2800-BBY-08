2cd5de6951c6bdb5f4d69c726dc03ede96f379e2
Chunki Lam <clam193@my.bcit.ca>
2024-05-16 14:51:11 -0700

    first draft of tuturial page is done, it functions well and more styling to be added

--- index.js
+++ index.js
@@ -7,27 +7,35 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: false }));
+
 app.set('view engine', 'ejs');
 
 /** Arrays of tutorial articles to be parsed from tutorial.json */
 let tutorialArray;
 
-fs.readFile('tutorial.json','utf-8', (err, data) =>{
-    if(err){
+fs.readFile('tutorial.json', 'utf-8', (err, data) => {
+    if (err) {
         console.error('Error reading file:', err);
         return;
     }
 
     try {
         tutorialArray = JSON.parse(data);
-        console.log(tutorialArray);
-    } catch (error){
+        console.log(tutorialArray[0]);
+    } catch (error) {
         console.error('Error parsing JSON', error);
     }
 });
 
 app.get('/tutorial', (req, res) => {
-    res.render("tutorial", {tutorialArray: tutorialArray});
+    res.render("tutorial", { tutorialArray: tutorialArray });
+});
+
+/** clickable article details. */
+app.post('/articles/:articleId', (req, res) => {
+    const articleId = req.params.articleId;
+   res.render("articles", { tutorialArray: tutorialArray, articleId });
 });
 
 app.listen(port, () => {
